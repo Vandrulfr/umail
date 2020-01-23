e5824c91d42cf65910735c6facbc4d4d56b0166e
Artur Melo <vandrulfr@github.com>
2020-01-23 15:43:54 -0300

    modelo aluno

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_22_202755) do
+ActiveRecord::Schema.define(version: 2020_01_23_184143) do
+
+  create_table "alunos", force: :cascade do |t|
+    t.string "nome"
+    t.string "email"
+    t.string "matricula"
+    t.string "telefone"
+    t.string "uffmail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
